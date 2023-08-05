9df9356e0e3cfe83d1279ea615e03033dd5a55e6
David Campbell <tryggus20@gmail.com>
2023-08-05 17:56:44 -0500

    completed stretch 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -49,10 +49,13 @@ for (let i=0; i<supplyChanges.length; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for (change of supplyChanges) {
+    console.log("Change of supply by", change);
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
